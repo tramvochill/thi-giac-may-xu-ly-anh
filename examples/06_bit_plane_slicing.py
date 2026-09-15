11348340f838baca4eed2c6c9edb35441eb631e0
Võ Thị Ngọc Trâm <191848807+tramvochill@users.noreply.github.com>
2026-09-16 00:01:20 +0700

    Use bit plane 6 for a distinct result

--- examples/06_bit_plane_slicing.py
+++ examples/06_bit_plane_slicing.py
@@ -2,6 +2,6 @@ from common import make_gray_sample, save_comparison, print_saved
 
 
 img = make_gray_sample()
-bit = 7
+bit = 6
 out = ((img >> bit) & 1) * 255
-print_saved(save_comparison(img, out, "06_bit_plane_slicing.png", "Input", "Bit plane 7"))
+print_saved(save_comparison(img, out, "06_bit_plane_slicing.png", "Input", "Bit plane 6"))
