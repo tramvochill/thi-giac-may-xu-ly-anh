67130f2fee3691f158aa66eebf3169ae06863350
tramvochill <vot227613@gmail.com>
2026-09-15 08:53:51 +0700

    Rename output image file for median filter

--- examples/10_median.py
+++ examples/10_median.py
@@ -3,4 +3,4 @@ from common import make_noisy_sample, median_filter, save_comparison, print_save
 
 img = make_noisy_sample()
 out = median_filter(img, 3)
-print_saved(save_comparison(img, out, "10_median_filter.png", "Salt-pepper noise", "3x3 median"))
+print_saved(save_comparison(img, out, "10_median.png", "Salt-pepper noise", "3x3 median"))
