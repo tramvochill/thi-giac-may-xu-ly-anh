2e6c108ac5a90279868cd5b6cf61f881310f603a
tramvochill <vot227613@gmail.com>
2026-09-15 08:54:03 +0700

    Rename output image file to '16_rgb.png'

--- examples/16_rgb.py
+++ examples/16_rgb.py
@@ -3,4 +3,4 @@ from common import make_color_sample, rgb_to_gray, save_comparison, print_saved
 
 img = make_color_sample()
 out = rgb_to_gray(img)
-print_saved(save_comparison(img, out, "16_rgb_model.png", "RGB input", "Luminosity gray"))
+print_saved(save_comparison(img, out, "16_rgb.png", "RGB input", "Luminosity gray"))
