e3bc1a561f65fd9c3e3b4246f9064b35b69f59de
tramvochill <vot227613@gmail.com>
2026-09-15 08:54:20 +0700

    Rename output image file to '18_hsi.png'

--- examples/18_hsi.py
+++ examples/18_hsi.py
@@ -4,4 +4,4 @@ from common import make_color_sample, rgb_to_hsi, clip_uint8, save_comparison, p
 rgb = make_color_sample()
 hsi = rgb_to_hsi(rgb)
 preview = clip_uint8(hsi * 255)
-print_saved(save_comparison(rgb, preview, "18_hsi_model.png", "RGB", "HSI as image"))
+print_saved(save_comparison(rgb, preview, "18_hsi.png", "RGB", "HSI as image"))
