94174fc0645add250d4ca1255be00398681d38ed
tramvochill <vot227613@gmail.com>
2026-09-15 08:52:32 +0700

    Rename 02 Log example

--- examples/02_log.py
+++ examples/02_log.py
@@ -5,4 +5,4 @@ from common import make_dark_sample, normalize_to_uint8, save_comparison, print_
 img = make_dark_sample()
 c = 255 / np.log(1 + 255)
 out = c * np.log(1 + img.astype(np.float32))
-print_saved(save_comparison(img, out, "02_log_transform.png", "Dark input", "Log transform"))
+print_saved(save_comparison(img, out, "02_log.png", "Dark input", "Log transform"))
